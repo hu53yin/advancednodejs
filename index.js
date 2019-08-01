2ae5eb5b5e9a8f0c2dd7d260176dcfbe52778d3b
Huseyin Gurkan <huseyingurkan@gmail.com>
2019-08-01 19:54:19 +0300

    clustering

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
-const express = require("express");
-const app = express();
+process.env.UV_THREADPOOL_SIZE = 2;
+const cluster = require("cluster");
 
-app.get("/", (req, res) => {
-  res.send("Hi there");
-});
+// Is the file being executed in master mode?
+if (cluster.isMaster) {
+  // Cause index.js to be executed *again* but
+  // in child mode
+  cluster.fork();
+  //   cluster.fork();
+  //   cluster.fork();
+  //   cluster.fork();
+} else {
+  // Im a child, Im going to act like a server
+  // and do nothing else
+  const express = require("express");
+  const crypto = require("crypto");
+  const app = express();
 
-app.listen(3000);
+  app.get("/", (req, res) => {
+    crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
+      res.send("Hi there");
+    });
+  });
+
+  app.get("/fast", (req, res) => {
+    res.send("This was fast");
+  });
+
+  app.listen(3000);
+}
